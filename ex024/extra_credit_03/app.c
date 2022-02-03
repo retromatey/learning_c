ff51c86a9f09924f4ac8baf35d288a756994bd5e
Jason Tennant <jason.tennant@outlook.com>
2022-02-03 13:08:42 +0000

    chp 24 additions

--- ex024/extra_credit_03/app.c
+++ ex024/extra_credit_03/app.c
@@ -3,6 +3,15 @@
 
 // Fix the file so that input names get stripped of the trailing newline
 // characters and any white space.
+// 
+// Dec Char                     
+// ─────────────────────────────
+// 0   NUL '\0' (null character)
+// 9   HT  '\t' (horizontal tab)
+// 10  LF  '\n' (new line)      
+// 13  CR  '\r' (carriage ret)  
+// 27  ESC (escape)             
+// 32  SPACE                    
 
 #define MAX_DATA 100
 
@@ -13,6 +22,14 @@ typedef enum EyeColor {
 
 const char *EYE_COLOR_NAMES[] = {
     "Blue", "Green", "Brown", "Black", "Other"
+};
+
+const int REMOVABLE_CHARS[] = {
+    9,  // \t (horizontal tab)
+    10, // \n (new line)
+    13, // \r (carriage ret)
+    27, // ESC (escape)
+    32  // SPACE
 };
 
 typedef struct Person {
@@ -29,6 +46,7 @@ int main(int argc, char *argv[]) {
     char *in = NULL;
 
     // From the man page for fgets
+    // char *fgets(char *s, int size, FILE *stream);
     // fgets() reads in at most one less than size characters from stream and
     // stores them into the buffer pointed to by s.  Reading stops after an EOF
     // or a newline.   If  a newline  is read, it is stored into the buffer.  A
@@ -36,9 +54,36 @@ int main(int argc, char *argv[]) {
     // buffer.
 
     // try to print out the variable in a loop to see each char?
-    printf("What's your First Name? ");
+    printf("Before input\n");
+    for (i = 0; i < MAX_DATA; i++) {
+        printf("%d %c %d | ", i, you.first_name[i], you.first_name[i]);
+    }
+
+    // Make a function to print out the string
+    // Make a function to do trim
+
+    //printf("What's your First Name? ");
+    printf("\nWhat's your First Name? ");
     in = fgets(you.first_name, MAX_DATA - 1, stdin);
     check(in != NULL, "Failed to read first name.");
+
+    printf("\nAfter input, before trim\n");
+    for (i = 0; i < MAX_DATA; i++) {
+        printf("%d %c %d | ", i, you.first_name[i], you.first_name[i]);
+    }
+
+    int j = 0;
+    for (i = 0; i < MAX_DATA; i++) {
+        for (j = 0; j < 5; j++) {
+            if ((int)you.first_name[i] == REMOVABLE_CHARS[j]) {
+                you.first_name[i] = 0;
+            }
+        }
+    }
+    printf("\n\nAfter trim\n");
+    for (i = 0; i < MAX_DATA; i++) {
+        printf("%d %c %d | ", i, you.first_name[i], you.first_name[i]);
+    }
 
     /*
     printf("What's your Last Name? ");
