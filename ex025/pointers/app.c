d9e083123fc596a6726df0afdfdcf277152b8190
Jason Tennant <jason.tennant@outlook.com>
2022-02-09 13:11:14 +0000

    chp 25 additions

--- ex025/pointers/app.c
+++ ex025/pointers/app.c
@@ -3,14 +3,62 @@
 #include <stdarg.h>
 #include "dbg.h"
 
+void change_var_value(int *p, int new_value) {
+    *p = new_value;
+    printf("\n------------------------------------------\n");
+    printf("Variable | Address        | Value\n");
+    printf("------------------------------------------\n");
+    printf("      *p | %p | %p\n", &p, p);
+    printf("------------------------------------------\n\n");
+}
+
 int main(int argc, char *argv[]) {
 
     int a = 100;
     int *ap = &a;
     int **app = &ap;
 
-    int b = 200;
+    //int b = 200;
+    int *b = NULL;
+    b = (int *)malloc(sizeof(int));
+    *b = 500;
+    printf("\n------------------------------------------\n");
+    printf("Variable | Address        | Value\n");
+    printf("------------------------------------------\n");
+    printf("      *b | %p | %p\n", &b, b);
+    printf("------------------------------------------\n\n");
+    printf("*b is %d", *b);
 
+
+    printf("\n------------------------------------------\n");
+    printf("Variable | Address        | Value\n");
+    printf("------------------------------------------\n");
+    printf("       a | %p | %d\n", &a, a);
+    printf("     *ap | %p | %p\n", &ap, ap);
+    printf("    *app | %p | %p\n", &app, app);
+    printf("------------------------------------------\n\n");
+
+    printf("running change_var_value(&a, 200)\n");
+    change_var_value(&a, 200);
+    printf("\n------------------------------------------\n");
+    printf("Variable | Address        | Value\n");
+    printf("------------------------------------------\n");
+    printf("       a | %p | %d\n", &a, a);
+    printf("     *ap | %p | %p\n", &ap, ap);
+    printf("    *app | %p | %p\n", &app, app);
+    printf("------------------------------------------\n\n");
+
+    printf("running change_var_value(ap, 300)\n");
+    change_var_value(ap, 300);
+    printf("\n------------------------------------------\n");
+    printf("Variable | Address        | Value\n");
+    printf("------------------------------------------\n");
+    printf("       a | %p | %d\n", &a, a);
+    printf("     *ap | %p | %p\n", &ap, ap);
+    printf("    *app | %p | %p\n", &app, app);
+    printf("------------------------------------------\n\n");
+
+    /*
     printf("  a: %d\n", a);
     printf("*ap: %d\n", *ap);
     printf(" &a: %p\n", &a);
@@ -33,6 +81,7 @@ int main(int argc, char *argv[]) {
     printf("*ap: %d\n", *ap);
     printf(" &a: %p\n", &a);
     printf(" ap: %p\n", ap);
+    */
 
     return 0;
 }
